096422f783e88e42fa451f575c9fa6b2773fe917
Anghel941 <80909584+Anghel941@users.noreply.github.com>
2021-03-28 11:00:39 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,34 +1,6 @@
 
-let slider =document.querySelector(".slider_contenedor")
-let sliderIndividual =document.querySelectorAll(".contenido_slider")
-let contador=1;
-let width= sliderIndividual[0].clientWidth;
-let intervalo = 8000;
 
-window.addEventListener("resize",function(){
-    width= sliderIndividual[0].clientWidth;
-})
-
-setInterval(function(){
-     slides();
-},intervalo);
-
-function slides(){
-    slider.style.transform= "translate("+(-(width)*contador)+"px)";
-    slider.style.transition= "transform .8s";
-    contador++;
-
-    if(contador==sliderIndividual.length){
-        setTimeout(function(){
-            slider.style.transform= "translate(0px)";
-            slider.style.transition= "transform 0s";
-            contador=1;
-        },1500)
-    }
-}
-
-
-/*bara de menu oculto*/
+/*barra de menu oculto*/
 
 let ubicacion_principal=window.pageYOffset;
 
@@ -41,8 +13,7 @@ window.addEventListener("scroll",function(){
     }
     ubicacion_principal=desplazamiento_actual;
 })
-/* menu */
-
+/* menu */ 
 let enlaces_menu= document.querySelectorAll(".menu")[0];
 let semaforo=true;
 
@@ -56,4 +27,4 @@ document.querySelectorAll(".hamburguer")[0].addEventListener("click",function(){
     }
     enlaces_menu.classList.toggle("menu-oculto")
 })
-
+ 
