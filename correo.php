dfc18121f7178f64ed06693bc3510169e5395170
Anghel941 <80909584+Anghel941@users.noreply.github.com>
2021-03-28 15:46:49 -0500

    Add files via upload

--- correo.php
+++ correo.php
@@ -1,9 +1,9 @@
 <?php 
-    $destinatario='edmelectronica21@gmail.com';//al correo a quien se envia el mensaje
-    $nombre=$_POST['nombre'];
-    $asunto=$_POST['asunto'];
-    $email=$_POST['correo'];
-    $mensaje=$_POST['mensaje'];
+    $destinatario="clickjaenoficial@gmail.com";//al correo a quien se envia el mensaje
+    $nombre=$_POST["nombre"];
+    $asunto=$_POST["asunto"];
+    $email=$_POST["correo"];
+    $mensaje=$_POST["mensaje"];
 
     $header= "Enviado desde la pagina Web RADOSYS";
     $mensajecompleto = $mensaje."\nAtentamente: " .$nombre;
@@ -12,5 +12,5 @@
     //enviar correo
     mail($destinatario,$asunto,$mensajecompleto,$header);
     echo"<script>alert('Correo enviado correctamente.')</script>";
-    echo"<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+    echo"<script> setTimeout(\"location.href='index.html'\",1000)</script>";
 ?>
